0d72d958095ae058493d117ef78f9dadd254b1a2
Omar Ahmed <omarking05@gmail.com>
2022-07-05 11:26:44 +0200

    feat(utils): return random pokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,17 @@
-import { trpc } from "@/utils/trpc";
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: 'Omar' }]);
-
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
-
-  if (data) {
-    return <div>{ data.greeting }</div>
-  }
+  const [firstId, secondId] = getOptionsForVote();
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200">First</div>
+        <div className="w-16 h-16 bg-red-800">{firstId}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200">Second</div>
+        <div className="w-16 h-16 bg-red-800">{secondId}</div>
       </div>
     </div>
   );
