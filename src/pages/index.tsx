ff89ebb987883e638de23b51ec822b6b5a69de18
Omar Ahmed <omarking05@gmail.com>
2022-07-05 19:37:32 +0200

    feat(index): use next image loader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import Image from "next/image";
+import type { NextPage } from "next";
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
-import type { NextPage } from "next";
 import { inferQueryResponse } from "./api/trpc/[trpc]";
 
 const btn = "inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -20,7 +21,6 @@ const Home: NextPage = () => {
   }
 
   const voteForRoundest = (selected: number) => {
-    // TODO: fire mutation to persist changes
     if (selected === first) {
       voteMutation.mutate({ votedFor: first, votedAgainst: second });
     } else {
@@ -28,7 +28,6 @@ const Home: NextPage = () => {
     }
     updateIds(getOptionsForVote());
   }
-
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
@@ -53,7 +52,7 @@ type PokemonFromServer = inferQueryResponse<"get-pokemon-by-id">;
 const PokemonListing: React.FC<{pokemon: PokemonFromServer, vote: () => void}> = ({ pokemon, vote }) => {
   return (
     <div className="w-64 h-64 flex flex-col items-center">
-      <img alt={pokemon.name} src={pokemon.sprites.front_default} className="w-64 h-64" />
+      <Image alt={pokemon.name} width={256} height={256} src={pokemon.sprites.front_default} layout="fixed" />
       <div className="text-xl text-center capitalize mt-[-2rem]">
         {pokemon.name}
       </div>
