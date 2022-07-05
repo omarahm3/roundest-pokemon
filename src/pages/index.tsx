0c2e2aa7f558aee3bb76f063b81d88ec31d2afd7
Omar Ahmed <omarking05@gmail.com>
2022-07-05 10:10:03 +0200

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="text-2xl text-red-200">
+      Hello World
+    </div>
+  );
+};
+
+export default Home;
