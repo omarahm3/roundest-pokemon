9034b24d2ef478882962f13104c62dbf8f932956
Omar Ahmed <omarking05@gmail.com>
2022-07-05 10:29:44 +0200

    feat(home): basic UI structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,14 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="text-2xl text-red-200">
-      Hello World
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200">First</div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200">Second</div>
+      </div>
     </div>
   );
 };
